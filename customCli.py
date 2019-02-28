56f267adf16826315221d59c532e50a36a4e9acd
unknown <bbashemera@gmail.com>
2019-02-28 19:01:29 +0300

    feat:Second commit

--- customCli.py
+++ customCli.py
@@ -3,9 +3,10 @@ import requests
 
 
 @click.command()
-def ancho():
+def anchor():
    
-    Welcome()    
+    Welcome()
+    getApiData()
 
 def Welcome():
     click.echo('\n')
@@ -17,4 +18,17 @@ def Welcome():
     click.echo(click.style('*' + ' ' * 98 + '*', fg='red'))
     click.echo(click.style('*' * 100, fg='yellow'))
     click.echo('\n')
-cli()
+def getApiData():
+    news_request = requests.get(
+        "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=45d024f423f94974a94342bc7b1d6fd6")
+    news_dictionary = news_request.json()
+    articles =news_dictionary['articles']
+    
+    for article in articles[:4] :
+        click.echo(click.style('TITLE: ' + article['title'], fg='yellow'))
+        click.echo(click.style('BY: ' + article['author'], fg='green'))
+        click.echo('\n')
+        click.echo(click.wrap_text(article['description'], 100))
+        click.echo('\n')
+        click.echo('-' * 100)
+anchor()
